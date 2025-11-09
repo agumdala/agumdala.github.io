cb031ecbbdc33bd9d7838007e7b2fc068b5aa9b4
Arnav Gumdala <agumdala@gmail.com>
2025-11-09 12:53:03 -0800

    Title name edit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="Title">
+        <h1>Coming Soon</h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
